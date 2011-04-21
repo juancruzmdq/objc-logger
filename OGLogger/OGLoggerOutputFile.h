6b7376b93ed26d04aba045cdf4d8e9050ad07fa1
Juan Cruz Ghigliani <juancruzmdq@gmail.com>
2011-04-21 19:10:46 -0300

    Include project files

---
+++ OGLogger/OGLoggerOutputFile.h
@@ -0,0 +1,16 @@
+//
+//  OGLoggerOutputFile.h
+//  objc-logger
+//
+//  Created by Juan Cruz Ghigliani on 20/04/11.
+//  Copyright 2011 Juan Cruz Ghigliani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OGLoggerOutput.h"
+
+@interface OGLoggerOutputFile : OGLoggerOutput {
+
+}
+
+@end
